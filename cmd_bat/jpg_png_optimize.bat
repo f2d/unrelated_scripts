3c2a17f5d8ce6df20feabd2b6a8f6c679d3be660
f2d <from2d@gmail.com>
2020-07-29 00:35:58 +0300

    Add help, remove wildcards.
    - Show help with any usual help-asking argument, e.g. "-h" or "/?".
    - Use for loops instead of wildcards for running without specified files.

--- cmd_bat/jpg_png_optimize.bat
+++ cmd_bat/jpg_png_optimize.bat
@@ -1,34 +1,62 @@
 
 @echo off
 
-rem * Description: try to run optimizers over PNG and JPEG files.
-rem * Usage: drag and drop files onto the script icon in Explorer. You may put this script on desktop or qucklaunch for ease of use.
+set "first_arg=%~1"
+set "first_arg=%first_arg:-=%"
+set "first_arg=%first_arg:/=%"
 
-rem * Warning: filenames with commas will not work due to how they are parsed (splitted) by cmd.
-rem * Note: Spaces, quotes, (parentheses), [brackets] and {braces} work normally though.
-
-rem * See these links to get the programs:
-rem * JpegOptim - https://github.com/tjko/jpegoptim
-rem * OptiPNG - http://optipng.sourceforge.net/
-rem * OxiPNG - https://github.com/shssoichiro/oxipng
-rem * TruePNG - http://x128.ho.ua/pngutils.html
-
-rem * Put the programs in any of these locations:
-rem * a) the same folder with this script, e.g. desktop.
-rem * b) %WINDIR%\SYSTEM32 e.g. C:\Windows\System32\.
-rem * c) any other folder from %PATH%.
-
-rem * Note: OxiPNG works but is not recommended, OptiPNG result filesize is better, except OxiPNG with -Z option which takes very long time.
-rem * Note: TruePNG works but is not recommended, result filesize is very good, but takes much more time.
-rem * Warning: TruePNG versions 0.6.2.2-0.6.2.4 with "/o max" option corrupt specific monocrome files. Fixed in version 0.6.2.5.
+if not "%first_arg%" == "help" ^
+if not "%first_arg%" == "h" ^
+if not "%first_arg%" == "?" ^
+goto start_processing
 
 
 
 
+ECHO:
+ECHO: * Description:
+ECHO:	Try to run optimizers over PNG and JPEG files.
+ECHO:	Default compression is not the most powerful available,
+ECHO:	because this script is intended for handy and quick manual use,
+ECHO:	e.g. just before showing your files on the web without waiting for too long.
+ECHO:
+ECHO: * Usage:
+ECHO:	a) Drag and drop files onto the script icon in Explorer. For ease of use, you may put this script on desktop or quicklaunch.
+ECHO:	b) Call it from command line without arguments to process all files in current working folder. For this, put it into some location from %%PATH%%.
+ECHO:
+ECHO: * Notes:
+ECHO:	Filenames with commas will not work due to how they are parsed (splitted) by cmd.
+ECHO:	Spaces, quotes, (parentheses), [brackets] and {braces} are OK though.
+ECHO:
+ECHO: * See these links to get the programs:
+ECHO:	JpegOptim - https://github.com/tjko/jpegoptim
+ECHO:	OptiPNG - http://optipng.sourceforge.net/
+ECHO:
+ECHO: * These programs are not used by default (to use them, edit this script and remove the "rem" text below):
+ECHO:	OxiPNG - https://github.com/shssoichiro/oxipng
+ECHO:	TruePNG - http://x128.ho.ua/pngutils.html
+ECHO:
+ECHO: * Put the programs in any of these locations:
+ECHO:	a) the same folder with this script, e.g. desktop.
+ECHO:	b) %%WINDIR%%\SYSTEM32 e.g. %WINDIR%\SYSTEM32\.
+ECHO:	c) any other folder from %%PATH%%: %PATH%.
+ECHO:
+ECHO: * Notes:
+ECHO:	OxiPNG works but is not recommended, OptiPNG result filesize is better, except OxiPNG with -Z option which takes very long time.
+ECHO:	TruePNG works but is not recommended, result filesize is better than OptiPNG, but takes significantly more time.
+ECHO:	TruePNG versions 0.6.2.2-0.6.2.4 with "/o max" option can corrupt specific monocrome files. Fixed in version 0.6.2.5.
+
+goto end_after_pause
+
+
+
+
+:start_processing
 set j=JpegOptim --all-progressive
 set p=OptiPNG -v -i 0 -fix
-rem set p=OxiPNG -v -i 0 --fix
+rem set p=OxiPNG -v -i 0 --fix -Z
 rem set p=TruePNG /o max /md keep text
+
 
 
 
@@ -56,9 +84,11 @@ goto end
 echo ---- Processing all files in current folder: ----
 @echo on
 
-%j% *.jpeg
-%j% *.jpg
-%p% *.png
+@FOR %%I IN (*.jpeg) DO %j% "%%I"
+@FOR %%I IN (*.jpg)  DO %j% "%%I"
+@FOR %%I IN (*.png)  DO %p% "%%I"
+
+@echo off
 
 
 
@@ -67,4 +97,6 @@ echo ---- Processing all files in current folder: ----
 echo ---- Processing complete. ----
 @echo on
 
+:end_pause
 pause
+:end_after_pause
