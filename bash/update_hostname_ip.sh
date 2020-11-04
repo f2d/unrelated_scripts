ed3c17c5ef4c6beb2f17693977d457a783cea38a
f2d <from2d@gmail.com>
2020-11-05 02:09:03 +0300

    Fix IP regexes to avoid replacing partial match

--- bash/update_hostname_ip.sh
+++ bash/update_hostname_ip.sh
@@ -2,26 +2,28 @@
 
 if [ -z "${update_hosts_file}" ]; then update_hosts_file=true; fi
 if [ -z "${hosts_file_path}"   ]; then hosts_file_path=/etc/hosts; fi
-if [ -z "${target_hostname}"   ]; then target_hostname=ftp.example.com; fi
+if [ -z "${target_hostname}"   ]; then target_hostname=back.2draw.me; fi
 
-ip_pattern='^[0-9\.]+$'
+ip_pattern="[0-9]+([.][0-9]+)*"
 
 if [ "${update_hosts_file}" == "true" ]
 then
 	# https://unix.stackexchange.com/a/20793
 	# 1) dig queries DNS servers directly, does not look at /etc/hosts/NSS/etc:
-	target_ip=`dig +short $target_hostname | awk '/^[0-9]+\./ { print ; exit }'`
+	target_ip=`dig +short ${target_hostname} | awk '/^[0-9]+[.]/ { print ; exit }'`
 else
 	# 2) getent, which comes with glibc, resolves using gethostbyaddr/gethostbyname2, and so also will check /etc/hosts/NIS/etc:
-	target_ip=`getent ahosts $target_hostname | awk '/^[0-9]+\./ { print $1; exit }'`
+	target_ip=`getent ahosts ${target_hostname} | awk '/^[0-9]+[.]/ { print $1; exit }'`
 fi
 
-if [[ "$target_ip" =~ $ip_pattern ]]
+if [[ "${target_ip}" =~ ^${ip_pattern}$ ]]
 then
 	if [ "${update_hosts_file}" == "true" ]
 	then
-		# https://gist.github.com/Fuxy22/da4b7ca3bcb0bfea2c582964eafeb4ed
-		if [ -n "$(grep ${target_hostname} ${hosts_file_path})" ]
+		hostname_pattern_in_hosts="[[:blank:]]+${target_hostname//[.]/[.]}([[:space:]]|$)"
+		hostname_found_in_hosts=`grep -P "${hostname_pattern_in_hosts}" "${hosts_file_path}"`
+
+		if [ -n "${hostname_found_in_hosts}" ]
 		then
 			echo "Hostname ${target_hostname} found in ${hosts_file_path}, replacing IP with ${target_ip}"
 
@@ -29,7 +31,7 @@ then
 				sed
 				--in-place=.bak
 				--regexp-extended
-				"s/(^|[\r\n])[ \t]*\b[0-9\.]+([ \t]+${target_hostname}\b)/\1${target_ip}\2/"
+				"s/(^|[\r\n])[[:blank:]]*${ip_pattern}(${hostname_pattern_in_hosts})/\1${target_ip}\3/"
 				"${hosts_file_path}"
 			)
 
