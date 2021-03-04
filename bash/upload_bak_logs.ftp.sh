a193800db704279f253392d271c82e0924014601
f2d <from2d@gmail.com>
2021-03-04 09:32:29 +0300

    FTPS: enable P mode initially

--- bash/upload_bak_logs.ftp.sh
+++ bash/upload_bak_logs.ftp.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 
+if [ "${ftp_protocol}" == "ftps" ]
+then
+	ftp_protocol_cmd=set ftps:initial-prot P
+else
+	ftp_protocol_cmd=
+fi
+
 echo -e "
+${ftp_protocol_cmd}
 connect ${ftp_protocol}://${ftp_username}:${ftp_password}@${ftp_hostname}/
 ls
 mput *.gz.tar
