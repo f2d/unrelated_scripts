5c7d9f535d325f006e9c0f6f66ecd7ab60e83537
f2d <from2d@gmail.com>
2022-04-23 04:19:25 +0300

    pip_update_all.py: Use colored text

--- python/2-3/pip_update_all.py
+++ python/2-3/pip_update_all.py
@@ -2,6 +2,17 @@
 # -*- coding: UTF-8 -*-
 # Python 2 or 3 should work.
 
+# Use colored text if available:
+try:
+	from termcolor import colored, cprint
+	import colorama
+
+	colorama.init()
+
+except ImportError:
+	def colored(*list_args, **keyword_args): return list_args[0]
+	def cprint(*list_args, **keyword_args): print(list_args[0])
+
 # Check versions of python and pip:
 # https://stackoverflow.com/a/9079062
 
@@ -12,17 +23,34 @@ python_version_string = str(sys.version_info.major)
 # pip_exe_filename = 'pip' + python_version_string
 pip_exe_filename = 'python' + python_version_string + ' -m pip'
 
-print('Got python version: ' + python_version_string + ', full text:\n' + sys.version + '\n')
+print(
+	colored('Got python version: ', 'yellow')
++	python_version_string
++	colored(', full text:\n', 'yellow')
++	sys.version
++	'\n'
+)
 
 cmd_string = pip_exe_filename + ' --version'
-print('Running: ' + cmd_string + '\n')
+
+print(
+	colored('Running: ', 'magenta')
++	cmd_string
++	'\n'
+)
 
 pip_version_output = subprocess.check_output(cmd_string, shell=True)
 pip_version_output = pip_version_output.decode().strip()
 pip_version_string = pip_version_output.split(' ')[1]
 pip_version_numbers = list(map(int, pip_version_string.split('.')))
 
-print('Got pip version: ' + pip_version_string + ', full text:\n' + pip_version_output + '\n')
+print(
+	colored('Got pip version: ', 'yellow')
++	pip_version_string
++	colored(', full text:\n', 'yellow')
++	pip_version_output
++	'\n'
+)
 
 # Update packages:
 # https://stackoverflow.com/a/5839291
@@ -36,19 +64,25 @@ or	(
 	and	(len(pip_version_numbers) < 3 or pip_version_numbers[2] < 1)
 	)
 ):
-	print('Get packages for pip version <= 10.0.0:\n')
+	cprint('Get packages for pip version <= 10.0.0:\n', 'yellow')
 
 	import pip
 	packages = [dist.project_name for dist in pip.get_installed_distributions()]
 else:
-	print('Get packages for pip version >= 10.0.1:\n')
+	cprint('Get packages for pip version >= 10.0.1:\n', 'yellow')
 
 	import pkg_resources
 	packages = [dist.project_name for dist in pkg_resources.working_set]
 
 cmd_string = pip_exe_filename + ' install --upgrade ' + ' '.join(packages)
-print('Command to run: ' + cmd_string + '\n')
-print('Press Enter to continue.')
+
+print(
+	colored('Command to run: ', 'magenta')
++	cmd_string
++	'\n'
+)
+
+cprint('Press Enter to continue.', 'cyan')
 
 if sys.version_info.major == 2:
 	raw_input()
@@ -56,3 +90,5 @@ else:
 	input()
 
 subprocess.call(cmd_string, shell=True)
+
+cprint('\nDone.', 'green')
