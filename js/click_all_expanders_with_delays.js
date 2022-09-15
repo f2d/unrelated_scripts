c0dcc8e4e9503c164f05a671714af96a89898494
f2d <from2d@gmail.com>
2022-09-15 15:51:43 +0300

    click_all_expanders_with_delays.js: Repeat until nothing found

--- js/click_all_expanders_with_delays.js
+++ js/click_all_expanders_with_delays.js
@@ -5,13 +5,23 @@ Copypaste this script into web-browser console,
 It will expand all visible collapsed branches of comment tree, one by one with delays to allow loading.
 If needed, repeat by pressing [Up] in the console and then [Enter] key again, until the total shows "0".
 
-CSS classes (bottom list) and link texts (top list) are written for specific sites like vk.com, naked-science.ru and pikabu.ru.
+Target CSS classes and text content parts are written for specific sites like vk.com, naked-science.ru and pikabu.ru.
 Change these lists as needed for other sites.
 */
 
 (function() {
-	var linksTotalCount = 0;
-	var linkTexts = [
+
+	var targetCssClasses = [
+		'comment-toggle-children_collapse',
+		'comment-hidden-group__toggle',
+		'story__read-more-label',
+		'shesht-comments-block-form-readmore',
+		'toggle-comment',
+		'wall_post_more',
+		'wall_reply_more',
+	];
+
+	var targetTextParts = [
 		'See more',
 		'Показать полностью',
 		'еще комментари',
@@ -19,66 +29,70 @@ Change these lists as needed for other sites.
 		'раскрыть ветку',
 	];
 
-	function getClickCallback(e,i) { return (function() {
-		if (e) {
-			console.log(
-				i
-			+	' / '
-			+	linksTotalCount
-			+	' - '
-			+	e.textContent
-				.replace(/^\s+|\s+$/g, '')
-				.replace(/\s+/g, ' ')
-			);
+	function openAllVisibleExpanders() {
+		var linksTotalCount = 0;
 
-			e.click();
-		}
-	}); }
+		function getClickCallback(e,i) { return (function() {
+			if (e) {
+				console.log(
+					i
+				+	' / '
+				+	linksTotalCount
+				+	' - '
+				+	e.textContent
+					.replace(/^\s+|\s+$/g, '')
+					.replace(/\s+/g, ' ')
+				);
 
-	for (var className of arguments) {
-		var linksOfClassCount = 0;
-		var linkTextContent;
-
-		forEachLinkElement:
-		for (var linkElement of document.getElementsByClassName(className)) {
-
-			var testElement = linkElement;
-
-			while (testElement) if (
-				testElement.hidden
-			||	(testElement.style && testElement.style.display == 'none')
-			) {
-				continue forEachLinkElement;
-			} else {
-				testElement = testElement.parentNode;
+				e.click();
 			}
 
-			if (linkTextContent = linkElement.textContent) {
-				for (var linkText of linkTexts)
-				if (linkTextContent.indexOf(linkText) >= 0) {
-					++linksOfClassCount;
-					++linksTotalCount;
+			if (i === linksTotalCount) {
+				console.log('Retrying...');
 
-					setTimeout(
-						getClickCallback(linkElement, linksTotalCount),
-						linksTotalCount * 300 + Math.random() * 200
-					);
+				setTimeout(openAllVisibleExpanders, 1234);
+			}
+		}); }
 
-					break;
+		for (var className of targetCssClasses) {
+			var linksOfClassCount = 0;
+			var linkTextContent;
+
+			forEachLinkElement:
+			for (var linkElement of document.getElementsByClassName(className)) {
+
+				var testElement = linkElement;
+
+				while (testElement) if (
+					testElement.hidden
+				||	(testElement.style && testElement.style.display == 'none')
+				) {
+					continue forEachLinkElement;
+				} else {
+					testElement = testElement.parentNode;
+				}
+
+				if (linkTextContent = linkElement.textContent) {
+					for (var linkText of targetTextParts)
+					if (linkTextContent.indexOf(linkText) >= 0) {
+						++linksOfClassCount;
+						++linksTotalCount;
+
+						setTimeout(
+							getClickCallback(linkElement, linksTotalCount),
+							linksTotalCount * 300 + Math.random() * 200
+						);
+
+						break;
+					}
 				}
 			}
+
+			if (linksOfClassCount > 0) console.log(linksOfClassCount+' '+className);
 		}
 
-		if (linksOfClassCount > 0) console.log(linksOfClassCount+' '+className);
+		console.log('Total '+linksTotalCount);
 	}
 
-	console.log('total '+linksTotalCount);
-})(
-	'comment-toggle-children_collapse',
-	'comment-hidden-group__toggle',
-	'story__read-more-label',
-	'shesht-comments-block-form-readmore',
-	'toggle-comment',
-	'wall_post_more',
-	'wall_reply_more'
-);
+	openAllVisibleExpanders();
+})();
