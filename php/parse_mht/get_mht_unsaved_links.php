deba542e15b8c00fb3b89debc112d6d491184632
f2d <from2d@gmail.com>
2023-08-16 21:42:54 +0300

    get_mht_unsaved_links.php: Show command line progress
    Looks better than as if it hangs for minutes.
    Uses less memory for command output processing in coming parts instead of all at once.

--- php/parse_mht/get_mht_unsaved_links.php
+++ php/parse_mht/get_mht_unsaved_links.php
@@ -14,8 +14,11 @@ $program_arg = '"C:\Program Files\7-Zip\7z.exe"';
 ini_set('max_execution_time', 9999);
 ini_set('error_reporting', E_ALL);
 
+define('PIPE', true);
 define('TEST', false);
+define('TEST_PIPE_SPLITS', true);
 
+define('NL', "\n");
 define('A_HREF_PAT', '~(?:^|\s)href="?([^"]+)~i');
 define('A_TEXT_PAT', '~(?:^|>)([^<>]+)(?:$|<)~i');
 define('P_HASH_LENGTH', 32);
@@ -31,6 +34,12 @@ $return_codes = array(
 ,	7 => 'Command line error.'
 ,	8 => 'Not enough memory.'
 ,	255 => 'User stopped the process.'
+);
+
+$descriptor_spec = array(
+	0 => array('pipe', 'r')	//* stdin is a pipe that the child will read from
+,	1 => array('pipe', 'w')	//* stdout is a pipe that the child will write to
+,	2 => array('pipe', 'w')	//* stderr is a pipe that the child will write to
 );
 
 $saved_page_hashes = array();
@@ -84,7 +93,10 @@ function add_saved_page ($page_url) {
 function add_linked_page ($text) {
 	global $saved_page_hashes, $saved_page_ids, $saved_pages, $linked_pages;
 
-	if ($page_entry = get_a_parts(html_entity_decode($text))) {
+	if (
+		($decoded_text = html_entity_decode($text))
+	&&	($page_entry = get_a_parts($decoded_text))
+	) {
 
 		$page_id = $page_entry['id'];
 		$page_hash = $page_entry['hash'];
@@ -108,6 +120,8 @@ function add_linked_page ($text) {
 		foreach ($pages[$page_key] as $found_entry) if ($page_title === $found_entry['title']) {
 			return false;
 		}
+
+		$page_entry['source_text'] = htmlspecialchars($decoded_text);
 
 		array_push($pages[$page_key], $page_entry);
 	}
@@ -134,75 +148,151 @@ function print_block ($var) {
 }
 
 function run_test ($command_line, $filter_func_name = '', $map_func_name = '', $split_each_line_by = '') {
-	global $return_codes;
+	global $descriptor_spec, $return_codes;
 
 	flush();
 
-	$output_lines = array();
-	$return_code = 0;
-	$return_text = exec($command_line, $output_lines, $return_code);
+	if (PIPE) {
+		$process = proc_open($command_line, $descriptor_spec, $pipes);
+		$stdout = $pipes[1];
+
+		$output_buffer = '';
+		$output_lines = 0;
+		$return_code = 0;
+		$flush_time = 0;
+
+		if (!$split_each_line_by) {
+			$split_each_line_by = NL;
+		}
+
+		$splitter_length = strlen($split_each_line_by);
+
+		while (
+			is_string($output_part = stream_get_line($stdout, 0))
+		&&	strlen($output_part)
+		) {
+			$output_buffer .= $output_part;
+			$split_lines = 0;
+
+			while (is_int($nearest_split_pos = strpos($output_buffer, $split_each_line_by))) {
+
+				$line = substr($output_buffer, 0, $nearest_split_pos);
+				$output_buffer = substr($output_buffer, $nearest_split_pos + $splitter_length);
+				$output_lines++;
+				$split_lines++;
+
+				if ($filter_func_name($line)) {
+					$map_func_name($line);
+
+					if (TEST_PIPE_SPLITS) echo '+';
+				} else	if (TEST_PIPE_SPLITS) echo '-';
+			}
+
+			if (TEST_PIPE_SPLITS) {
+				if (
+					!is_int($nearest_split_pos)
+				&&	strlen($output_buffer)
+				) {
+					echo ($split_lines ? NL.'|' : '|');
+				}
+			} else {
+				echo ($split_lines ? NL.'.' : '.');
+			}
+
+			$read_time = time();
+
+			if ($flush_time !== $read_time) {
+				$flush_time = $read_time;
+
+				flush();
+			}
+		}
+
+		if (strlen($output_buffer)) {
+			$output_lines++;
+
+			if ($filter_func_name($output_buffer)) {
+				$map_func_name($output_buffer);
+
+				if (TEST_PIPE_SPLITS) echo 'V';
+			} else	if (TEST_PIPE_SPLITS) echo 'X';
+		} else		if (TEST_PIPE_SPLITS) echo '0';
+
+		$process_status = proc_get_status($process);
+		$return_code = $process_status['exitcode'];
+
+//* Close any pipes before calling proc_close in order to avoid a deadlock:
+
+		foreach ($pipes as $pipe) if ($pipe) fclose($pipe);
+
+		proc_close($process);
+	} else {
+		$output_lines = array();
+		$return_code = 0;
+		$return_text = exec($command_line, $output_lines, $return_code);
+
+		if (TEST) {
+			if ($split_each_line_by) {
+				if (is_array($output_lines)) {
+
+//* https://stackoverflow.com/questions/526556/how-to-flatten-a-multi-dimensional-array-to-simple-one-in-php#comment98422873_14972714
+//* array_merge(...): https://stackoverflow.com/a/53891086
+//* Note: this can hit memory limit on large enough set of files (e.g. 100-1000 or more). Only do this for testing and debug.
+
+					$output_lines = array_merge(
+						...array_map(
+							function  ($line) use ($split_each_line_by) {
+								return explode($split_each_line_by, $line);
+							}, $output_lines
+						)
+					);
+				} else {
+					$output_lines = explode($split_each_line_by, "$output_lines");
+				}
+			}
+
+			if (is_array($output_lines)) {
+
+				if ($filter_func_name) {
+					$output_lines = array_filter($output_lines, $filter_func_name);
+				}
+
+				if ($map_func_name) {
+					$output_lines = array_map($map_func_name, $output_lines);
+				}
+
+				$output_lines = array_filter($output_lines, 'trim');
+				$output_lines = 'Array('.count($output_lines).')'.NL.implode(NL, array_map('get_to_print', $output_lines));
+			}
+		} else {
+
+//* Note: to avoid memory limit, do only necessary things and move on.
+
+			if ($split_each_line_by) {
+				foreach ($output_lines as $text) {
+					foreach (explode($split_each_line_by, $text) as $line) {
+						if ($filter_func_name($line)) {
+							$map_func_name($line);
+						}
+					}
+				}
+			} else {
+				foreach ($output_lines as $line) {
+					if ($filter_func_name($line)) {
+						$map_func_name($line);
+					}
+				}
+			}
+
+			$output_lines = 'Array('.count($output_lines).')';
+		}
+	}
 
 	$return_code = "$return_code: ".(
 		array_key_exists($return_code, $return_codes)
 		? $return_codes[$return_code]
 		: 'Unknown error.'
 	);
-
-	if (TEST) {
-		if ($split_each_line_by) {
-			if (is_array($output_lines)) {
-
-//* https://stackoverflow.com/questions/526556/how-to-flatten-a-multi-dimensional-array-to-simple-one-in-php#comment98422873_14972714
-//* array_merge(...): https://stackoverflow.com/a/53891086
-//* Note: this can hit memory limit on large enough set of files (e.g. 100-1000 or more). Only do this for testing and debug.
-
-				$output_lines = array_merge(
-					...array_map(
-						function  ($line) use ($split_each_line_by) {
-							return explode($split_each_line_by, $line);
-						}, $output_lines
-					)
-				);
-			} else {
-				$output_lines = explode($split_each_line_by, "$output_lines");
-			}
-		}
-
-		if (is_array($output_lines)) {
-
-			if ($filter_func_name) {
-				$output_lines = array_filter($output_lines, $filter_func_name);
-			}
-
-			if ($map_func_name) {
-				$output_lines = array_map($map_func_name, $output_lines);
-			}
-
-			$output_lines = array_filter($output_lines, 'trim');
-			$output_lines = 'Array('.count($output_lines).")\n".implode("\n", array_map('get_to_print', $output_lines));
-		}
-	} else {
-
-//* Note: to avoid memory limit, do only necessary things and move on.
-
-		if ($split_each_line_by) {
-			foreach ($output_lines as $text) {
-				foreach (explode($split_each_line_by, $text) as $line) {
-					if ($filter_func_name($line)) {
-						$map_func_name($line);
-					}
-				}
-			}
-		} else {
-			foreach ($output_lines as $line) {
-				if ($filter_func_name($line)) {
-					$map_func_name($line);
-				}
-			}
-		}
-
-		$output_lines = 'Array('.count($output_lines).')';
-	}
 
 	print_block("
 		command_line = $command_line
@@ -262,7 +352,7 @@ if ($linked_pages) {
 		foreach ($linked_pages[$page_key] as $page_entry) {
 			echo "
 		<br>$page_entry[rubric], $page_entry[hash], $page_entry[id], $page_entry[time] -
-		<a href=\"$page_entry[url]\">$page_entry[title]</a>";
+		<a href=\"$page_entry[url]\" title=\"$page_entry[source_text]\">$page_entry[title]</a>";
 		}
 
 		echo "\n	</p>";
