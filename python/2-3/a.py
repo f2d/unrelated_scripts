503ab6f98dd0ecd29e223897b537d9c1ed0b4333
f2d <from2d@gmail.com>
2024-12-02 06:03:49 +0300

    a.py: Check if content sum size is not zero for compression ratio

--- python/2-3/a.py
+++ python/2-3/a.py
@@ -1245,7 +1245,7 @@ def run_batch_archiving(argv):
 					summary_parts = archive_file_summary.split(' ', 4)
 					content_sum_size = int(summary_parts[2])
 					content_counts_text = summary_parts[4]
-					compression_ratio = 100.0 * archive_file_size / content_sum_size
+					compression_ratio = (100.0 * archive_file_size / content_sum_size) if content_sum_size > 0 else 0
 
 					archive_size_text = get_bytes_text(archive_file_size)
 					content_size_text = get_bytes_text(content_sum_size)
